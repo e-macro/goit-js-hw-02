c6883b112161d9d831581376cf9be6313e37949f
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-03-31 18:05:35 +0300

    .

--- js/task-4.js
+++ js/task-4.js
@@ -7,7 +7,7 @@ function getShippingCost(country) {
             return `Shipping to ${country} will cost 250 credits`;
             break;
         case "Australia":
-            return `Shipping to ${country} will cost 250 credits`;
+            return `Shipping to ${country} will cost 170 credits`;
             break;
         case "Jamaica":
             return `Shipping to ${country} will cost 120 credits`;
